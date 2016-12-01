a35e632b5b5219ca4e847597e039c5357b66792d
Prophet Xu <slowfishcolor@gmail.com>
2016-12-01 14:33:36 +0800

    solve No 206 & 383

---
+++ src/no206reverseLinkedList/Solution.java
@@ -0,0 +1,7 @@
+package no206reverseLinkedList;
+
+/**
+ * Created by Prophet on 2016/11/30.
+ */
+public class Solution {
+}
---
+++ src/no383ransomNote/Solution.java
@@ -0,0 +1,7 @@
+package no383ransomNote;
+
+/**
+ * Created by Prophet on 2016/12/1.
+ */
+public class Solution {
+}
