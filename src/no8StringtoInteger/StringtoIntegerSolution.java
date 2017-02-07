bca86dd1d5cc59bb3639dcfa8ebbef3cb94e3eec
Prophet Xu <slowfishcolor@gmail.com>
2017-02-07 10:53:43 +0800

    solve no8 String to Integer

---
+++ src/no8StringtoInteger/StringtoIntegerSolution.java
@@ -0,0 +1,34 @@
+package no8StringtoInteger;
+
+/**
+ * Created by Prophet on 2017/2/7.
+ */
+public class StringtoIntegerSolution {
+
+    private static final int maxdiv10 = Integer.MAX_VALUE / 10;
+
+    public int myAtoi(String str) {
+        int i = 0;
+        int n = str.length();
+        while(i < n && Character.isWhitespace(str.charAt(i))) i++;
+
+        int sign = 1;
+        if (i < n && str.charAt(i) == '+') {
+            i++;
+        } else if (i < n && str.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        int num = 0;
+        while (i < n && Character.isDigit(str.charAt(i))) {
+            int digit = Character.getNumericValue(str.charAt(i));
+            if (num > maxdiv10 || (num == maxdiv10 && digit >= 8)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            num = num * 10 + digit;
+            i++;
+        }
+        return sign * num;
+    }
+}
