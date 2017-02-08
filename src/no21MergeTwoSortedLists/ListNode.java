a9a7a78fbb21d34dc4a8f769f03c2f94e2b55d72
Prophet Xu <slowfishcolor@gmail.com>
2017-02-08 15:18:02 +0800

    solve no 21 Merge Two Sorted Lists

---
+++ src/no21MergeTwoSortedLists/ListNode.java
@@ -0,0 +1,13 @@
+package no21MergeTwoSortedLists;
+
+/**
+ * Created by Prophet on 2017/2/8.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+
+}
